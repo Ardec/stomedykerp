76a7837ee71d60305ce007bf2387c92edb400b61
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2023-12-28 05:51:19 +0100

    fetch

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div>
     BASE URL
     <ClientOnly>
-      !{{ base_url }}
+      ??{{ baseUrl }}
     </ClientOnly>
     
     <!-- <NuxtWelcome /> -->
@@ -10,7 +10,26 @@
 </template>
 
 <script setup>
-const base_url = process.env.QUASAR_DATA;
-console.log('Base URL:', base_url);
+import { ref, onMounted } from 'vue';
+import { useNuxtApp } from '#app';
+
+const { $http } = useNuxtApp();
+const config = useRuntimeConfig();
+const baseUrl = config.public.DATABASE_ID;
+const categories = ref([]);
+
+const load_categories = async () => {
+  try {
+    const response = await $http.get(`${baseUrl}/min-categories`);
+    categories.value = response.data.data.simpleCategories;
+    categories.value.forEach(element => {
+      element.searchEngine = se_read[element.searchEngine - 1]; // Upewnij się, że `se_read` jest zdefiniowane
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(load_categories);
 
 </script>
