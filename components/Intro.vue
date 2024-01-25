7a63e69fff7050ee05f1232fa60b77f2638954d0
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2024-01-25 07:47:29 +0100

    add_file

---
+++ components/Intro.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="center-container">
+    <img class="animate__animated animate__bounce logo-container" src="~/assets/img/kdev.png" alt="Discover Nuxt 3" />
+    <h2>K-Development Startup for ERP & CRM</h2>
+  </div>
+</template>
+
+<style>
+.center-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh; /* Ustawia wysokość kontenera na 100% widocznej wysokości ekranu */
+  text-align: center; /* Wyśrodkowanie tekstu dla wszystkich elementów wewnątrz kontenera */
+}
+</style>
