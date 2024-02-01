fbfd85b4d1301cc0237dad63cafad8b856079814
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2024-02-01 19:41:31 +0100

    working-modal

--- components/ShowModal.vue
+++ components/ShowModal.vue
@@ -5,19 +5,26 @@
         <template #header>
           <div class="flex items-center justify-between">
             <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-              Modal2
+              {{ $attrs?.item.name }}
             </h3>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="closeModal()" />
           </div>
         </template>
         <Placeholder class="h-32" />
+          {{ $attrs?.item }}
       </UCard>
     </UModal>
     </div>
 </template>
 
 <script setup>
+const emit = defineEmits(['close-modal']);
+const attrs = useAttrs();
+let isOpen = ref(false);
+isOpen = computed(() => attrs.isOpen);
 
-const isOpen = ref(false)
+function closeModal() {
+  emit('close-modal');
+}
 
 </script>
